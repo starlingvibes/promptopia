d1936774f0515a09daba28dc20d627ee07508334
starlingvibes <starlingroot@protonmail.com>
2023-05-30 11:52:37 +0100

    fix(): add feed component & import provider

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed';
+
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
@@ -11,7 +13,7 @@ const Home = () => {
         discover, create and share creative prompts
       </p>
 
-      {/* Feed component */}
+      <Feed />
     </section>
   );
 };
