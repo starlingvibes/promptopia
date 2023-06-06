b7b3c6c75a9789414a56d6b35a1954e1ceac8859
starlingvibes <starlingroot@protonmail.com>
2023-06-06 15:53:04 +0100

    fix(): setup auth providers and nav elements

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,77 @@
-import React from 'react';
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+
+    setProviders();
+  }, []);
+
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='Promptopia logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>Promptopia</p>
+      </Link>
+      {/* Desktop navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <>
+            <div className='flex gap-3 md:gap-5'>
+              <Link href='/create-prompt' className='black_btn'>
+                Create post
+              </Link>
+              <button type='button' onClick={signOut} className='outline_btn'>
+                Sign out
+              </button>
+              <Link href='/profile'>
+                <Image
+                  src='/assets/images/logo.svg'
+                  width={37}
+                  height={37}
+                  className='rounded-full'
+                  alt='Profile'
+                ></Image>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  onClick={() => signIn(provider.id)}
+                  className='outline_btn'
+                  key={provider.name}
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
