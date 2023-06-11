2ba65b176e5ff093453d66cdfdef2a9b5c6ecd05
starlingvibes <starlingroot@protonmail.com>
2023-06-11 13:41:39 +0100

    chore(): making fetch request to new prompt API

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -16,6 +16,16 @@ const CreatePrompt = () => {
   const createPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+    } catch (error) {}
   };
 
   return (
