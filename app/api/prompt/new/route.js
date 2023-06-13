56052d1fe06c51e6d41cc9d50f672d1632e9dbda
starlingvibes <starlingroot@protonmail.com>
2023-06-13 15:37:11 +0100

    feat(): getting the create prompt functionality working

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,1 +1,22 @@
-export const POST = async (req, res) => {};
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const POST = async (req, res) => {
+  const { userId, prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response(JSON.stringify('Failed to create a new prompt'), {
+      status: 500,
+    });
+  }
+};
