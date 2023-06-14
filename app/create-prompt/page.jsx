717d748b06613dac155dcc6ece8b923d81098742
starlingvibes <starlingroot@protonmail.com>
2023-06-14 12:15:45 +0100

    chore(): setting up the create prompt route

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+  return <Form />;
+};
+
+export default CreatePrompt;
--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectToDB = async () => {
-  // mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
   if (isConnected) {
     console.log('MongoDB is already connected');
